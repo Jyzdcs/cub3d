82d29ea4620b5238accc9a9a38a351e500f7dd7d
Jyzdcs <ky.claudant@gmail.com>
2025-10-02 18:12:33 +0200

    Suppression de plusieurs fichiers objets de la bibliothèque `libft` et ajout de la fonction `get_next_line` dans `get_next_line.c`. Mise à jour de l'en-tête `libft.h` pour inclure la déclaration de `get_next_line` et définir `BUFFER_SIZE` par défaut.

--- libs/libft/libft.h
+++ libs/libft/libft.h
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kclaudan <kclaudan@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+
+	+:+     */
+/*   By: kclaudan <kclaudan@student.42.fr>          +#+  +:+
+	+#+        */
+/*                                                +#+#+#+#+#+
+	+#+           */
 /*   Created: 2024/10/24 15:45:25 by kclaudan          #+#    #+#             */
 /*   Updated: 2024/10/24 15:45:25 by kclaudan         ###   ########.fr       */
 /*                                                                            */
@@ -12,17 +15,22 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stdlib.h>
 # include <stdint.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 typedef struct s_list
 {
-	void			*content;
-	struct s_list	*next;
-}				t_list;
+	void *content;
+	struct s_list *next;
+} t_list;
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -40,18 +48,18 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *str);
-int		ft_atoi(const char *str);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_lstsize(t_list *lst);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_lstsize(t_list *lst);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, unsigned int size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
@@ -64,7 +72,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_itoa(int nb);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-t_list	*ft_lstnew(void	*content);
+t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 
 #endif
