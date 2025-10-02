386777f2ce5a5daed0fcc7b133f9bf2b66c42e9b
Jyzdcs <ky.claudant@gmail.com>
2025-10-02 13:02:25 +0200

    Ajout des fichiers d'en-tête cub3d.h et main.c, définissant les structures de données pour le jeu Cub3D, y compris les textures, le joueur, la carte et le point d'entrée principal du programme.

---
+++ include/cub3d.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kclaudan <kclaudan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 12:01:21 by kclaudan          #+#    #+#             */
+/*   Updated: 2025/10/02 12:51:51 by kclaudan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "controls.h"
+#include "graphics.h"
+#include "map_parser.h"
+#include "utils.h"
+
+typedef struct s_texture
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_texture;
+
+typedef struct s_player
+{
+	int			x;
+	int			y;
+	int			old_x;
+	int			old_y;
+	int			width;
+	int			height;
+	int			movements;
+	t_texture	img;
+}				t_player;
+
+typedef struct s_map
+{
+	void		*mlx;
+	void		*mlx_win;
+	char		**map;
+	t_texture	no_wall;
+	t_texture	so_wall;
+	t_texture	we_wall;
+	t_texture	ea_wall;
+	t_texture	floor;
+	t_texture	ceiling;
+}				t_map;
+
+typedef struct s_game
+{
+	t_map		map;
+	t_player	player;
+}				t_game;
