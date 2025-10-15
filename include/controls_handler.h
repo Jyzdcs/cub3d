f72699f42e9408f70971b45a2735bf28174c7da3
kaa <kaazarikuso@gmail.com>
2025-10-15 20:41:42 +0200

    add keys camera movement logic + unit tests

--- include/controls_handler.h
+++ include/controls_handler.h
@@ -3,17 +3,20 @@
 
 # include <X11/keysym.h>
 # include <X11/X.h>
+# include <math.h>
 # include "cub3d.h"
 
-int	handle_key_press(int keycode, t_game *game);
-int	handle_key_release(int keycode, t_game *game);
-int	there_is_no_wall(t_map *map, double x, double y);
+int		handle_key_press(int keycode, t_game *game);
+int		handle_key_release(int keycode, t_game *game);
+int		there_is_no_wall(t_map *map, double x, double y);
 void	move_forward(t_game *game);
 void	move_backward(t_game *game);
 void	strafe_left(t_game *game);
 void	strafe_right(t_game *game);
 void	rotate_left(t_game *game);
 void	rotate_right(t_game *game);
-
+void	rotate_player(t_game *game, double angle);
+void	rotate_left(t_game *game);
+void	rotate_right(t_game *game);
 
 #endif
