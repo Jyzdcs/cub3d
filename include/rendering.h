61849258e81bf179748d99158426253a5a74933a
kaa vm <kallajsubd@gmail.com>
2025-10-24 21:09:07 +0000

    fix a rendering issue

--- include/rendering.h
+++ include/rendering.h
@@ -5,13 +5,13 @@
 
 t_texture	*get_texture(t_game *game, int texture_id);
 void		draw_pixel(t_game *game, int x, int y, int color);
-void		draw_wall_column(t_game *game, int x, int draw_start, int draw_end);
+void		draw_wall_column(t_game *game, int x, t_render_frame *frame);
 void		rendering_frame(t_game *game);
 void		render_floor_and_ceiling(t_game *game);
 int			get_texture_pixel(t_texture *texture, int tex_x, int tex_y);
 void		init_texture(t_game *game, t_texture **texture, int *tex_x);
 void		clear_screen(t_game *game);
 void		init_wall_column(t_wall_column *wall, t_texture *texture,
-				int draw_start, int draw_end);
+				t_render_frame *frame);
 
 #endif
--- src/rendering/rendering.c
+++ src/rendering/rendering.c
@@ -8,35 +8,39 @@ void	init_texture(t_game *game, t_texture **texture, int *tex_x)
 	if (!*texture)
 		return ;
 	*tex_x = (int)(game->ray.wall_x * (double)(*texture)->width);
-	if (game->ray.side == 0 && game->ray.dir_x > 0)
-		*tex_x = (*texture)->width - *tex_x - 1;
-	else if (game->ray.side == 1 && game->ray.dir_y < 0)
-		*tex_x = (*texture)->width - *tex_x - 1;
+	if (*tex_x < 0)
+		*tex_x = 0;
+	if (*tex_x >= (*texture)->width)
+		*tex_x = (*texture)->width - 1;
 }
 
 void	init_wall_column(t_wall_column *wall, t_texture *texture,
-				int draw_start, int draw_end)
+			t_render_frame *frame)
 {
-	wall->step = 1.0 * texture->height
-		/ (draw_end - draw_start);
-	wall->draw_start = draw_start;
-	wall->draw_end = draw_end;
-	wall->tex_pos = 0;
+	wall->step = 1.0 * texture->height / frame->line_height;
+	wall->draw_start = frame->draw_start;
+	wall->draw_end = frame->draw_end;
+	wall->tex_pos = (frame->draw_start
+			- (-frame->line_height / 2 + SCREEN_HEIGHT / 2)) * wall->step;
 	wall->tex_y = 0;
 	wall->color = 0;
-	wall->y = draw_start;
+	wall->y = frame->draw_start;
 }
 
-void	draw_wall_column(t_game *game, int x, int draw_start, int draw_end)
+void	draw_wall_column(t_game *game, int x, t_render_frame *frame)
 {
 	t_wall_column	wall;
 	t_texture		*texture;
 
 	init_texture(game, &texture, &wall.tex_x);
-	init_wall_column(&wall, texture, draw_start, draw_end);
-	while (wall.y <= draw_end)
+	init_wall_column(&wall, texture, frame);
+	while (wall.y <= frame->draw_end)
 	{
-		wall.tex_y = (int)wall.tex_pos & (texture->height - 1);
+		wall.tex_y = (int)wall.tex_pos;
+		if (wall.tex_y < 0)
+			wall.tex_y = 0;
+		if (wall.tex_y >= texture->height)
+			wall.tex_y = texture->height - 1;
 		wall.tex_pos += wall.step;
 		wall.color = get_texture_pixel(texture, wall.tex_x, wall.tex_y);
 		draw_pixel(game, x, wall.y, wall.color);
@@ -44,9 +48,15 @@ void	draw_wall_column(t_game *game, int x, int draw_start, int draw_end)
 	}
 }
 
-void	clear_screen(t_game *game)
+void	init_frame(t_render_frame *frame, t_ray *ray)
 {
-	ft_memset(game->render_addr, 0, SCREEN_HEIGHT * game->render_line_length);
+	frame->line_height = (int)(SCREEN_HEIGHT / ray->perp_wall_dist);
+	frame->draw_start = -frame->line_height / 2 + SCREEN_HEIGHT / 2;
+	frame->draw_end = frame->line_height / 2 + SCREEN_HEIGHT / 2;
+	if (frame->draw_start < 0)
+		frame->draw_start = 0;
+	if (frame->draw_end >= SCREEN_HEIGHT)
+		frame->draw_end = SCREEN_HEIGHT - 1;
 }
 
 void	rendering_frame(t_game *game)
@@ -55,7 +65,6 @@ void	rendering_frame(t_game *game)
 	t_render_frame	frame;
 	int				x;
 
-	clear_screen(game);
 	render_floor_and_ceiling(game);
 	x = 0;
 	while (x < SCREEN_WIDTH)
@@ -63,14 +72,8 @@ void	rendering_frame(t_game *game)
 		init_ray(&ray, game, x);
 		cast_ray(&ray, game);
 		game->ray = ray;
-		frame.line_height = (int)(SCREEN_HEIGHT / ray.perp_wall_dist);
-		frame.draw_start = -frame.line_height / 2 + SCREEN_HEIGHT / 2;
-		frame.draw_end = frame.line_height / 2 + SCREEN_HEIGHT / 2;
-		if (frame.draw_start < 0)
-			frame.draw_start = 0;
-		if (frame.draw_end >= SCREEN_HEIGHT)
-			frame.draw_end = SCREEN_HEIGHT - 1;
-		draw_wall_column(game, x, frame.draw_start, frame.draw_end);
+		init_frame(&frame, &ray);
+		draw_wall_column(game, x, &frame);
 		x++;
 	}
 	mlx_put_image_to_window(game->mlx, game->mlx_win, game->render_img, 0, 0);
